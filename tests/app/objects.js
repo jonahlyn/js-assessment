c965abc03b6bb222230914cc31aca78990e8cab3
Jonahlyn Gilstrap <jgilstrap@gmail.com>
2012-04-23 09:25:01 -0600

    Added missing semicolons.

--- tests/app/objects.js
+++ tests/app/objects.js
@@ -28,7 +28,7 @@ define([ 'use!underscore' ], function(_) {
     it("you should be able to alter the context in which a method runs", function() {
       fn = function(){
         return a.sayIt.apply(b);
-      }
+      };
     
       // define a function for fn so that the following will pass
       expect(fn()).to.be('Yo, Rebecca!');
@@ -37,7 +37,7 @@ define([ 'use!underscore' ], function(_) {
     it("you should be able to alter multiple objects at once", function() {
       fn = function(greeting){
         C.prototype['greeting'] = greeting;
-      }
+      };
     
       // define a function for fn so that the following will pass
       var obj1 = new C('Rebecca'),
@@ -62,7 +62,7 @@ define([ 'use!underscore' ], function(_) {
         }
         
         return props;
-      }
+      };
       
       // define a function for fn so that the following will pass
       var C = function() {
